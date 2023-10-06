81e14ff333ed28dff31051d37eed15688575fde9
= <=>
2023-10-06 12:18:02 +0530

    templates

--- templates.cc
+++ templates.cc
@@ -1,16 +1,11 @@
 #include <iostream>
 #include <array>
 
-void print_array(std::array<int, 3> arr)
-{
-    for (auto value : arr)
-    {
-        std::cout << value << ' ';
-    }
-    std::cout << '\n';
-}
-
-void print_array(std::array<float, 3> arr)
+/*
+    Newer versions (c++ 20) support auto instead of the template
+*/
+template <typename T>
+void print_array(T arr)
 {
     for (auto value : arr)
     {
@@ -22,7 +17,9 @@ void print_array(std::array<float, 3> arr)
 int main()
 {
     std::array<int, 3> my_array_1{1, 2, 3};
-    print_array(my_array_1);
     std::array<float, 3> my_array_2{4.14, 5.1, 6.4};
+    print_array<std::array<int, 3>>(my_array_1);
+    // No need to specify it's type
     print_array(my_array_2);
+    return 0;
 }
